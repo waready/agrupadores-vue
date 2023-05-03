5437a3c61fe3bc223ac67ff500685181913441b5
waready <elbarto119@hotmail.com>
2023-05-03 00:06:37 -0500

    arreglos

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,18 +1,10 @@
 <template>
   <div>
-    <VueSidebarMenuAkahon
-     :isUsedVueRouter="true"
-      @menu-item-clicked="fnRouteTo(link)"
-      style="margin-top: 58px"
-      :isMenuOpen="true"
-      :menuTitle="'Agrupadores'"
-      :menuIcon="''"
-      :bgColor="'#3e2c42'"
-      :isSearch="false"
-      :menuItems="menuItems"
-      ref="sidebarMenu">
+    <VueSidebarMenuAkahon :isUsedVueRouter="true" @menu-item-clicked="fnRouteTo(link)" style="margin-top: 58px"
+      :isMenuOpen="true" :menuTitle="'Agrupadores'" :menuIcon="''" :bgColor="'#3e2c42'" :isSearch="false"
+      :menuItems="menuItems" ref="sidebarMenu">
     </VueSidebarMenuAkahon>
-       <!--Main layout-->
+    <!--Main layout-->
     <main style="margin-top: 58px">
       <div class="container pt-4"></div>
     </main>
@@ -28,34 +20,35 @@ export default {
   mounted() {
     this.$refs.sidebarMenu.$el.querySelector('#my-scroll').style.margin = '6px 0px 0px';
     this.$refs.sidebarMenu.$el.querySelector('#my-scroll .nav-list').style.paddingLeft = '0.7rem';
+    
   },
   components: { VueSidebarMenuAkahon },
   data() {
     return {
-     menuItems: [
-    {
-      link: '/indicador/100',
-      name: 'Condiciones Generales',
-      tooltip: 'Condiciones Generales',
-      icon: 'bx-grid-alt',
-    },
-    {
-      link: '/indicador/200',
-      name: 'Cajas Sucursales',
-      tooltip: 'Cajas Sucursales',
-      icon: 'bx-user',
-      
-    },
-    {
-      link: '/indicador/300',
-      name: 'Contabilidad',
-      tooltip: 'Contabilidad',
-      icon: 'bx-chat',
-    },
-  
-   ]
+      menuItems: [
+        {
+          link: '/indicador/100',
+          name: 'Condiciones Generales',
+          tooltip: 'Condiciones Generales',
+          icon: 'bx-grid-alt',
+        },
+        {
+          link: '/indicador/200',
+          name: 'Cajas Sucursales',
+          tooltip: 'Cajas Sucursales',
+          icon: 'bx-building',
+
+        },
+        {
+          link: '/indicador/300',
+          name: 'Contabilidad',
+          tooltip: 'Contabilidad',
+          icon: 'bx-file',
+        },
+
+      ]
+    }
   }
-}
 }
 </script>
 <style >
